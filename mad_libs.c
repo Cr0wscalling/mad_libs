f147a2164dbc0d35d0dcaa8c185711e9eb8965bd
Cr0wscalling <gitcr0wscalling@gmail.com>
2016-01-18 15:06:34 -0800

    DEL scanf items

--- mad_libs.c
+++ mad_libs.c
@@ -2,7 +2,7 @@
 * This C program elicits parts of speech from the user and 
 * incoporates them via string formatters into a into a paragraph of Sherlockian narrative.
 *
-* requires calling <cs50.h> that can be found https://manual.cs50.net/library/ or https://ide.cs50.io/
+* requires calling <cs50.h> that can be found https://manual.cs50.net/library/ or use whttps://ide.cs50.io/
 *
 * Base text adapted from "A Study in Scarlett" by Sir Arthur Conan Doyle 
 * accessed at http://www.gutenberg.org/files/244/244-h/244-h.htm#link2HCH
@@ -17,39 +17,38 @@ int main(void)
 	//obtains user input
 
 string name = GetString("Please enter a name: ");
-scanf
+
 
 int num = GetInt("Please enter a number: ");
-scanf
+
 
 string adjective_a = raw_input("Please enter adjective: ");
-scanf
+
 
 string noun_a = GetString("Please enter a noun: ");
-scanf
+
 
 string verb = GetString("Please enter a verb: ");
-scanf
+
 
 printf("Just a few more inquiries!");
 
 string noun_b = GetString("Please enter a second noun: ");
-scanf
 
 string person_pet = GetString("Please enter a type of person or pet: ");
-scanf
+
 
 printf("Almost done!");
 string noun_c = GetString("Please enter another noun: ");
-scanf
+
 
 // noun_b again
 
 string adjective_b = GetString("Please enter a second adjective: ");
-scanf
+
 
 string noun_c = GetString("Please enter a third noun: ");
-scanf
+
 
 // prints madlib with user input
 printf(\n
